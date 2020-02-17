3046a72155335f42f1ea890766600d02e1bcea27
GowthamArputharaj <61113685+GowthamArputharaj@users.noreply.github.com>
2020-02-17 22:40:04 +0530

    Add files via upload

---
+++ Auto_FaceBook_Login.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import pyautogui
+import sys
+import time
+file=open('D://Person//Facebook.csv','a')
+driver=webdriver.Chrome('C:\\Users\\Gowtham\\Downloads\\chromedriver_win32\\chromedriver.exe')
+driver.maximize_window()
+driver.get('https://www.facebook.com')
+us=driver.find_element_by_id('email')
+us.send_keys('gow1999th@gmail.com')
+pa=driver.find_element_by_id('pass')
+pa.send_keys('iidciede11DNA')
+cl=driver.find_element_by_xpath("//form[@id='login_form']/input[3]")
+cl.submit()
+b=pyautogui.locateOnScreen('D://facebook_notif_block.png')
+pyautogui.click(b)
+a=input('Enter quit if you want to close WebDriver/t ')
+if(a=='quit'):
+    a=pyautogui.locateOnScreen('D://drop.png')
+    print('Found Drop down list box for Log out')
+    print(a)
+    pyautogui.click(a)
+    time.sleep(3)
+    a=pyautogui.locateOnScreen('D://LogOut.png')
+    print('Found Log Out button for Log out')
+    print(a)
+    pyautogui.click(a)
+    print('facebook is signing out')
+    driver.close()
+
---
+++ OpenCV3_Face_and_Eye_Detection_screenshot.py
@@ -0,0 +1,56 @@
+#"C:\Users\Gowtham\AppData\Local\Programs\Python\Python38\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml"
+
+import cv2
+import numpy as np
+import time
+import pyautogui
+
+eye_detection=cv2.CascadeClassifier('C:\\Users\\Gowtham\\AppData\\Local\\Programs\\Python\\Python38\\Lib\\site-packages\\cv2\\data\\haarcascade_eye.xml')
+
+face_detection=cv2.CascadeClassifier('C:\\Users\\Gowtham\\AppData\\Local\\Programs\\Python\\Python38\\Lib\\site-packages\\cv2\\data\\haarcascade_frontalface_default.xml')
+
+
+#im = cv2.VideoCapture(0)
+while True:
+    #im = cv2.VideoCapture(0)
+    for i in range(5,0,-1):
+        print(i)
+        time.sleep(1)
+    #ret, frame = im.read()
+    #print(ret)
+    frame=pyautogui.screenshot()
+    frame.save(r"C:\\Users\\Gowtham\\Desktop\\abb.jpg")
+    #cv2.imwrite("C:\\Users\\Gowtham\\Desktop\\abb.jpg",frame)
+    #cv2.imshow("C:\\Users\\Gowtham\\Desktop\\abb.jpg",frame)
+    #cv2.waitKey(0)
+    print("before gray")
+    img= cv2.imread("C:\\Users\\Gowtham\\Desktop\\abb.jpg")
+    #img = cv2.imread("C:\\Users\\Gowtham\\Desktop\\ab.jpg")
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print("after gray, before faces")
+
+    eyes = eye_detection.detectMultiScale(gray, 1.3, 5)
+    print("after faces, before for")
+    for (x,y,w,h) in eyes:
+        print("inside for, before rect")
+        img=cv2.rectangle(img,(x,y),(x+w, y+h),(255,0,0),3)
+        print("inside for, after rect")
+
+    faces = face_detection.detectMultiScale(gray, 1.3, 5)
+    print("after faces, before for")
+    for (x,y,w,h) in faces:
+        print("inside for, before rect")
+        img=cv2.rectangle(img,(x,y),(x+w, y+h),(255,0,0),3)
+        print("inside for, after rect")
+
+    cv2.imwrite('C:\\Users\\Gowtham\\Desktop\\Face_AB.jpg',img)
+    print("outside for, before imshow")
+    cv2.imshow('D.E.T.E.C.T.E.D',img)
+    #cv2.imshow('C.A.P.T.U.R.E.D',frame)
+    print("after imshow, before delay")
+    #time.sleep(10)
+    print("after delay")
+    cv2.waitKey(0)
+    im.release()
+cv2.destroyAllWindows()
+
